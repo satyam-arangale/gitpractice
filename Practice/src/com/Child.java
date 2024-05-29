d567207bca93ecad2aca98d66548d187e78e99e3
arang <arang@DESKTOP-OL3I3KS>
2024-05-29 09:36:49 +0530

    first commit

---
+++ Practice/src/com/Child.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Child extends Test {
+
+	public void m1() {
+
+		
+		System.out.println("Hello there");
+	}
+
+	public static void main(String[] args) {
+
+		Test t = new Child();
+		
+	}
+}
